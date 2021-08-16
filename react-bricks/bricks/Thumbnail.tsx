975d805fb2bac25ccc7b00f0d084c657cb4fd583
Kevin Gagnon <kevin.gagnon@broadsign.com>
2021-08-16 17:04:45 -0400

    Edit Thumbnail Brick

--- react-bricks/bricks/Thumbnail.tsx
+++ react-bricks/bricks/Thumbnail.tsx
@@ -1,23 +1,28 @@
-import React from 'react'
-import { types, Text } from 'react-bricks'
+import React from "react";
+import { types, Text, RichText } from "react-bricks";
 
 const Thumbnail: types.Brick = () => {
-  return (
-  	<div className="p-6 text-center">
-	  <Text
-	  	propName="title"
-		renderBlock={({children})=><h1 className="text-2xl font-bold">{children}</h1>}
-		placeholder="Type a title please"
-	  />
-  	</div>
-  )
-}
+	return (
+		<div className="p-6 text-center">
+			<Text propName="title" renderBlock={({ children }) => <h1 className="text-2xl font-bold">{children}</h1>} placeholder="Type a title please" />
+			<RichText
+				propName="description"
+				renderBlock={({ children }) => <p className="text-lg text-gray-500">{children}</p>}
+				placeholder="Type a description"
+				allowedFeatures={[types.RichTextFeatures.Bold, types.RichTextFeatures.Highlight]}
+			/>
+		</div>
+	);
+};
 
 Thumbnail.schema = {
-  name: 'thumbnail',
-  label: 'Thumbnail',
-  getDefaultProps: () => ({}),
-  sideEditProps: [],
-}
+	name: "thumbnail",
+	label: "Thumbnail",
+	getDefaultProps: () => ({
+		title: "Hello, world!",
+		description: "Lorem ipsum dolor sit amet.",
+	}),
+	sideEditProps: [],
+};
 
-export default Thumbnail
+export default Thumbnail;
