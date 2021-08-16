8f936bd5b69bf52ce5212e487205828ecc93548c
Kevin Gagnon <kevin.gagnon@broadsign.com>
2021-08-16 16:44:14 -0400

    Edit Thumbnail Brick

--- react-bricks/bricks/Thumbnail.tsx
+++ react-bricks/bricks/Thumbnail.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
-import { types } from 'react-bricks'
+import { types, Text } from 'react-bricks'
 
 const Thumbnail: types.Brick = () => {
-  return <div className="p-6 text-center">Thumbnail</div>
+  return (
+  	<div className="p-6 text-center">
+	  <Text
+	  	propName="title"
+		renderBlock={({children})=><h1 className="text-2xl font-bold">{children}</h1>}
+		placeholder="Type a title please"
+	  />
+  	</div>
+  )
 }
 
 Thumbnail.schema = {
