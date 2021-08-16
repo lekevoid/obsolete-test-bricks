c5dafc653835342deb9a64fa7450620aba1a673b
Kevin Gagnon <kevin.gagnon@broadsign.com>
2021-08-16 17:11:10 -0400

    Edit Thumbnail Brick

--- react-bricks/bricks/Thumbnail.tsx
+++ react-bricks/bricks/Thumbnail.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { types, Text, RichText } from "react-bricks";
 
 const Thumbnail: types.Brick = () => {
+	const { Bold, Highlight } = types.RichTextFeatures;
+
 	return (
 		<div className="p-6 text-center">
 			<Text propName="title" renderBlock={({ children }) => <h1 className="text-2xl font-bold">{children}</h1>} placeholder="Type a title please" />
@@ -9,7 +11,8 @@ const Thumbnail: types.Brick = () => {
 				propName="description"
 				renderBlock={({ children }) => <p className="text-lg text-gray-500">{children}</p>}
 				placeholder="Type a description"
-				allowedFeatures={[types.RichTextFeatures.Bold, types.RichTextFeatures.Highlight]}
+				allowedFeatures={[Bold, Highlight]}
+				renderHighlight={({ children }) => <span className="px-1 rounded bg-blue-200 text-blue-900">{children}</span>}
 			/>
 		</div>
 	);
