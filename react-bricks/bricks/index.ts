03453560635fdeea5c90c567ef532b32c2f8d222
Kevin Gagnon <kevin.gagnon@broadsign.com>
2021-08-17 16:33:49 -0400

    Add Hero Component

--- react-bricks/bricks/index.ts
+++ react-bricks/bricks/index.ts
@@ -1,9 +1,10 @@
 import { types } from "react-bricks";
 import { website } from "react-bricks-ui";
+import Hero from "./Hero";
 import HeroUnit from "./MyHeroUnit";
 import Thumbnail from "./Thumbnail";
 
 // React Bricks UI + Custom HeroUnit brick
-const bricks: types.Brick<any>[] = [...website, HeroUnit, Thumbnail];
+const bricks: types.Brick<any>[] = [...website, Hero, HeroUnit, Thumbnail];
 
 export default bricks;
